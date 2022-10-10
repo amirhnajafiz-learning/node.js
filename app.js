932d8641c02859602e2f46e1e0cdda6d948aa409
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-10 18:18:28 +0330

    add: hook project

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+// load environment variables
+require('dotenv').config();
+
+const orderRoutes = require('./routes/order');
+
+const app = express();
+
+
+// your app global middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// routing your http requests
+app.use('/api', orderRoutes);
+
+
+// `Not Found` request handler
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  throw error;
+});
+
+
+// thrown erros handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ message: error.message || 'Internal Server Error' });
+});
+
+
+// bootstrap your server
+app.listen(process.env.PORT, () => console.log('Server running on port ' + process.env.PORT));
